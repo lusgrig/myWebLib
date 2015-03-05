8d11c162755f118653256db2cbc6289cb7020fb4
Jeff <jsaras@retailreinvented.com>
2015-03-05 15:54:11 -0800

    pulled after class

--- client/table.js
+++ client/table.js
@@ -1,5 +1,25 @@
 Template.tTable.helpers({
   links: function () {
     return Links.find();
+  },
+
+  sEdit : function() {
+  	return Session.get('sEdit');
+  }
+
+});
+
+Template.tTable.events({
+  'click .remove': function () {
+    Links.remove(this._id);
+  },
+
+  'click .edit': function() {
+  	Session.set('sEdit', true);
+  },
+
+  'click .btnUpdate' : function() {
+  	Link.update({this._id})
+  	Session.set('sEdit', false);
   }
 });
