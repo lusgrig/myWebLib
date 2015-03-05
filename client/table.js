009345f6d5b6e5788a408e9e53a501222154dfd5
Philip Howley <howley.phil@gmail.com>
2015-03-04 21:31:55 -0800

    almost made update work but not yet

--- client/table.js
+++ client/table.js
@@ -1,11 +1,23 @@
 Template.tTable.helpers({
   links: function () {
     return Links.find();
+  },
+  sEdit: function() {
+    return Session.get('sEdit');
   }
 });
 
 Template.tTable.events({
   'click .remove': function () {
     Links.remove(this._id);
+  },
+  'click .edit': function() {
+    Session.set('sEdit', true);
+  },
+  'click .btnUpdate': function(evt) {
+    var urlForm = evt.target.url.value;
+    var tagForm = evt.target.tag.value;
+    Links.update(this._id, {url: urlForm, tag: tagForm});
+    Session.set('sEdit', false);
   }
 });
